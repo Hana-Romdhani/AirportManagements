56c534a7e2209b20014939ecb1ce345772b82399
HR98R <romdhani.hana@esprit.tn>
2024-01-23 17:57:25 +0100

    Création des entites

---
+++ AirportManagement/AM.ApplicationCore/Domain/Flight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Flight
+    {
+        public string Destination { get; set; }
+        public string Departure { get; set; }
+        public DateTime FlightDate { get; set; }
+       public DateTime  EffectiveArrival { get; set; }
+        public   int  EstimatedDiration { get; set; }
+        public Plane myplane { get; set; }
+        public ICollection<Passenger> passengers { get; set; }
+    }
+}
---
+++ AirportManagement/AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Passenger 
+    {
+
+        public int TelNumber { get; set; }
+        public string LastName { get; set;}
+        public string FirstName { get; set;}
+        public string EmailAddress { get; set;}
+        public string PasswordNumber { get; set;}
+        public DateTime BithDate { get; set;}
+        public ICollection<Flight> flights { get; set; }
+
+    }
+}
