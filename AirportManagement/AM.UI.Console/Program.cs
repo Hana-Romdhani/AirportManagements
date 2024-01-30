0b5a197b6a774a3a91ae7d9f20825c8871abca80
HR98R <romdhani.hana@esprit.tn>
2024-01-30 15:52:33 +0100

    Seance2

--- AirportManagement/AM.UI.Console/Program.cs
+++ AirportManagement/AM.UI.Console/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc"); Console.WriteLine("Hello, World!xdhdhdhdggcggc");
+void myFirstcode()
+{
+
+}
+void mySecondcode()
+{
+
+}
+void myThirdcode()
+{
+
+}
